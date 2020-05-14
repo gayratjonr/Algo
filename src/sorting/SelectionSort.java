9a921dd91a170467994861413bb62988b4415d94
Gayratjon Rayimjonov <54319356+gayratjonr@users.noreply.github.com>
2020-05-14 12:17:37 +0500

    Update SelectionSort.java

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -11,16 +11,21 @@ public class SelectionSort {
     }
 
     private void simpleSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            int index = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[index] > array[j]) {
-                    int temp = array[index];
-                    array[index] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
+         int n = arr.length; 
+        for (int i = 0; i < n-1; i++) 
+        { 
+            // Find the minimum element in unsorted array 
+            int min_idx = i; 
+            for (int j = i+1; j < n; j++) 
+                if (arr[j] < arr[min_idx]) 
+                    min_idx = j; 
+  
+            // Swap the found minimum element with the first 
+            // element 
+            int temp = arr[min_idx]; 
+            arr[min_idx] = arr[i]; 
+            arr[i] = temp; 
+        } 
     }
 
     private <T extends Comparable> void sort(T[] array){
